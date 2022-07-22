b914117e1af92779fcf86beb62bb095f9ebd3bad
anr4876 <dustjdanr@naver.com>
2022-07-22 16:06:22 +0900

    NomadWeather

--- App.js
+++ App.js
@@ -3,18 +3,57 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+<View style={styles.container}>
+
+<View style={styles.city}>
+  <Text style={styles.cityName}>Seoul</Text>
+</View>
+
+<View style={styles.weather}>
+  <View style={styles.day}>
+    <Text style={styles.temperature}>27</Text>
+    <Text style={styles.description}>Sunny</Text>
+  </View>
+
+</View>
+
+<StatusBar style="light" />
+</View>
+);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles = StyleSheet.create({ // 자동 완성 가능한 style 객체 생성 ... container를 전달
+// css 틀리면 놀랍게도 native에서는 오류를 알려준다 ...
+container : {
+  flex: 1,
+  backgroundColor: '#00ff00',
+},
+  cityName : {
+  color: "#000000",
+  fontSize: 54,
+  fontWeight: 'bold'
+},  
+  city : {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+},
+  weather : {
+  flex: 3,
+
+},
+  day : {
+  flex: 1,
+  alignItems: "center",
+
+}, 
+  temperature : {
+  marginTop: 50,
+  fontSize: 100,
+},
+  description : {
+  marginTop: -10,
+  fontSize: 60,
+}
 });
+
